c18eaf79a89a772ad2b929846d47a79795b29da8
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2023-07-07 13:43:43 +0530

    Add files via upload

---
+++ DOWNLOADER.sh
@@ -0,0 +1,48 @@
+ban() {
+echo -e "\033[1;92m"
+
+toilet -f font DOWNLOAD
+printf "\n\t\033[10m [::]\033[1;93m Youtube/Hotstar video and audio download\033[0m [::]\n"
+}
+ytv() {
+echo -e -n  "\033[1;92m Enter Youtube/Hotstar link : "
+read ya
+if [ $ya ];then
+cd /sdcard
+yt-dlp -o "%(series)s/%(season_number)s - %(season)s/%(episode_number)s - %(episode)s.%(ext)s" $ya
+fi
+} 
+yta() {
+echo -e -n  "\033[1;92m Enter Youtube/Hotstar link : "
+read yv
+if [ $yv ];then
+cd /sdcard
+yt-dlp -x $yv
+fi
+} 
+ot() {
+echo -e -n  "\033[1;92m Enter Youtube/Hotstar link : "
+read ot
+if [ $ot ];then
+cd /sdcard
+yt-dlp $ot
+fi
+} 
+
+menu() {
+ban
+printf "\n\033[1;92m[\033[0m1\033[1;92m]\033[1;93m Youtube/Hotstar Video"
+printf "\n\033[1;92m[\033[0m2\033[1;92m]\033[1;93m Youtube/Hotstar audio"
+printf "\n\033[1;92m[\033[0m3\033[1;92m]\033[1;93m Try other link"
+printf "\n\033[1;92m[\033[0m4\033[1;92m]\033[1;93m Exit\n\n\n"
+echo -e -n "Enter option : "
+read b
+case $b in
+1)ytv ;;
+2)yta ;;
+3)ot ;;
+4)exit ;;
+*)menu ;;
+esac
+}
+menu
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# YH-Downloader
+```
+cd $HOME
+apt update && apt upgrade
+apt install git python python-pip.figlet toilet
+pip install -U yt-dlp
+git clone https://github.com/rooted-cyber/YH-Downloader
+cd YH-Downloader
+bash setup.sh
+yth
+```
